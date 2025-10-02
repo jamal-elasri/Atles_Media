09272b99201d37f0725c8251aec457630c4511c5
jamal el asri <153444893+jamal-elasri@users.noreply.github.com>
2025-10-02 14:00:50 +0100

    Add files via upload

---
+++ Atles_media/duo-studio-main/script.js
@@ -0,0 +1,203 @@
+function init() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
+
+    
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+
+}
+
+init()
+
+var crsr = document.querySelector(".cursor")
+var main = document.querySelector(".main")
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x + 20+"px"
+    crsr.style.top = dets.y + 20+"px"
+})
+
+gsap.from(".page1 h1,.page1 h2", {
+    y: 10,
+    rotate: 10,
+    opacity: 0,
+    delay: 0.3,
+    duration: 0.7
+})
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page1 h1",
+        scroller: ".main",
+        // markers:true,
+        start: "top 27%",
+        end: "top 0",
+        scrub: 3
+    }
+})
+tl.to(".page1 h1", {
+    x: -100,
+}, "anim")
+tl.to(".page1 h2", {
+    x: 100
+}, "anim")
+tl.to(".page1 video", {
+    width: "90%"
+}, "anim")
+
+
+/*
+var tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page1 h1",
+        scroller: ".main",
+        // markers:true,
+        start: "top -115%",
+        end: "top -120%",
+        scrub: 3
+    }
+})
+tl2.to(".main", {
+    backgroundColor: "#fff",
+})
+
+var tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page1 h1",
+        scroller: ".main",
+        // markers:true,
+        start: "top -280%",
+        end: "top -300%",
+        scrub: 3
+    }
+})
+
+tl3.to(".main",{
+    backgroundColor:"#0F0D0D"
+}) */
+
+
+var boxes = document.querySelectorAll(".box")
+boxes.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        var att = elem.getAttribute("data-image")
+        crsr.style.width = "470px"
+        crsr.style.height = "370px"
+        crsr.style.borderRadius = "0"
+        crsr.style.backgroundImage = `url(${att})`
+    })
+    elem.addEventListener("mouseleave",function(){
+        elem.style.backgroundColor = "transparent"
+        crsr.style.width = "20px"
+        crsr.style.height = "20px"
+        crsr.style.borderRadius = "50%"
+        crsr.style.backgroundImage = `none`
+    })
+})
+
+var h4 = document.querySelectorAll("#nav h4")
+var purple = document.querySelector("#purple")
+h4.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        purple.style.display = "block"   
+        purple.style.opacity = "1"
+    })
+    elem.addEventListener("mouseleave",function(){
+        purple.style.display = "none"   
+        purple.style.opacity = "0"
+    })
+});
+
+// HEADER
+
+const header = document.querySelector("[data-header]");
+
+const activeElementOnScroll = function () {
+  if (window.scrollY > 50) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+window.addEventListener("scroll", activeElementOnScroll);
+
+/**
+ * NAVBAR
+ * navbar toggle for mobile
+ */
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].addEventListener(eventType, callback);
+    }
+  }
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggleBtn.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.navbar-link');
+
+const linkAction = () => {
+    const navbar = document.querySelector('[data-navbar]');
+    const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+    // When we click on each nav__link, we remove the active class from the navbar
+    navbar.classList.remove('active');
+
+    navToggleBtn.classList.remove('active');
+};
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+document.querySelectorAll('.popup').forEach(popup => {
+    popup.addEventListener('click', function() {
+        const imageSrc = this.getAttribute('data-images');
+        const imgElement = document.querySelector('.popup-images img');
+        imgElement.src = imageSrc;
+        document.querySelector('.popup-images').style.display = 'block';
+    });
+});
+
+document.querySelector('.popup-images span').addEventListener('click', function() {
+    const imgElement = document.querySelector('.popup-images img');
+    imgElement.src = '';
+    document.querySelector('.popup-images').style.display = 'none';
+});
+
+document.querySelector('.popup-images').addEventListener('click', function(e) {
+    const imgElement = document.querySelector('.popup-images img');
+    if (e.target !== imgElement && e.target !== document.querySelector('.popup-images span')) {
+        imgElement.src = '';
+        document.querySelector('.popup-images').style.display = 'none';
+    }
+});
